c3f9fb29b3e5bb43b4cfaf52b8bff2f3885ecb8b
Iulia Kalganova <iuleasha7@mail.ru>
2021-04-03 23:20:35 +0300

    тесты

--- src/store/user/user.test.js
+++ src/store/user/user.test.js
@@ -1,10 +1,11 @@
 import {AuthorizationStatus} from '../../constants/constant';
+import {requireAuthorization} from '../action';
 import {user} from './user';
 
 describe(`UserReducer`, () => {
   it(`Reducer should authorize user`, () => {
     const state = {authorizationStatus: AuthorizationStatus.NO_AUTH};
 
-    expect(user(state, AuthorizationStatus.AUTH)).toEqual({authorizationStatus: AuthorizationStatus.AUTH});
+    expect(user(state, requireAuthorization(AuthorizationStatus.AUTH))).toEqual({authorizationStatus: AuthorizationStatus.AUTH});
   });
 });
